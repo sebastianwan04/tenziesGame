d3d7f2ad18a49b3951464ab2f8675ae2a7ecc365
sebastian.wan04 <sebastian.wan04@gmail.com>
2022-05-30 12:20:46 +0200

    Added dice components and working reroll button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react"
+import Die from "./Die"
 
 export default function App() {
+    const [dice, setDice] = React.useState(allNewDice())
+
+    function allNewDice() {
+        const newDice = []
+        for(let i=0;i<10;i++){
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
+    const diceElements = dice.map(die => <Die value={die} />)
+
+    function rollDice() {
+        setDice(allNewDice())
+    }
+
     return (
-        <main></main>
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
     )
 }
